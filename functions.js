a54293e4f860027234555ebf13873acd62bf56ac
tineindenberken <tineevelien@hotmail.com>
2022-08-23 11:37:53 +0200

    done

--- functions.js
+++ functions.js
@@ -20,20 +20,39 @@ function getSchool () {
 
 const school = getSchool();
 
-console.log(getSchool);
+console.log(school);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function getCompliment () {
+  return "Lekker bezig met die functies, Jan!"
+}
+const comp = getCompliment();
 
+console.log(comp);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades () {
+  return [8, 8.5, 6, 7];
+}
 
+const number = getGrades();
+
+console.log(number);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails () {
+  return { firstname: 'Tine', lastname: 'Berken'}
+}
+
+const detail = getDetails();
+
+console.log(detail);
